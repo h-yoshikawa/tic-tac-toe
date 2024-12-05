ba0dfe3f38a1a9c0b6656fdd99137e404dbed701
h-yoshikawa <hirofumi.yoshikawa@legalontech.jp>
2024-12-05 23:16:32 +0900

    Square コンポーネントを追加

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import Square from './Square';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Square />
+  </StrictMode>
+);
