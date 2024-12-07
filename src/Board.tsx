5236c6804b06e929fabb73e6cba391ca542a3da0
h-yoshikawa <hirofumi.yoshikawa@legalontech.jp>
2024-12-07 13:06:44 +0900

    応用問題 2 を実装

--- src/Board.tsx
+++ src/Board.tsx
@@ -25,21 +25,18 @@ export const Board = ({ xIsNext, squares, onPlay }: Props) => {
   return (
     <>
       <div className="status">{status}</div>
-      <div className="board-row">
-        <Square value={squares[0]} onSquareClick={() => handleSquareClick(0)} />
-        <Square value={squares[1]} onSquareClick={() => handleSquareClick(1)} />
-        <Square value={squares[2]} onSquareClick={() => handleSquareClick(2)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[3]} onSquareClick={() => handleSquareClick(3)} />
-        <Square value={squares[4]} onSquareClick={() => handleSquareClick(4)} />
-        <Square value={squares[5]} onSquareClick={() => handleSquareClick(5)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[6]} onSquareClick={() => handleSquareClick(6)} />
-        <Square value={squares[7]} onSquareClick={() => handleSquareClick(7)} />
-        <Square value={squares[8]} onSquareClick={() => handleSquareClick(8)} />
-      </div>
+      {[...Array(3).keys()].map((i) => {
+        const rows = [...Array(3).keys()].map((j) => {
+          const n = 3 * i + j;
+          return <Square key={n} value={squares[n]} onSquareClick={() => handleSquareClick(n)} />;
+        });
+
+        return (
+          <div key={i} className="board-row">
+            {rows}
+          </div>
+        );
+      })}
     </>
   );
 };
